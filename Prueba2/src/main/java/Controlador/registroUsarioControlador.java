a2f2413aa4c4fcfca1d93300fc6bb9d9b4b03b00
OtonielMendez00010023 <00010023@uca.edu.sv>
2026-06-18 16:08:32 -0600

    feat: Se actualiza clase fxml, mainfx y css

--- Prueba2/src/main/java/Controlador/registroUsarioControlador.java
+++ Prueba2/src/main/java/Controlador/registroUsarioControlador.java
@@ -2,14 +2,28 @@ package Controlador;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class registroUsarioControlador {
-    @FXML
-    private Label welcomeLabel;
+
+    @FXML private TextField tfUsuario;
+    @FXML private TextField tfEmail;
+    @FXML private PasswordField pfPassword;
+    @FXML private TextField tfFechaNacimiento;
+    @FXML private Label lblMensajeRegistro;
+
+    @FXML private TextField tfUsuarioVerificar;
+    @FXML private PasswordField pfPasswordVerificar;
+    @FXML private Label lblMensajeVerificar;
 
     @FXML
-    private void handleButton() {
-        welcomeLabel.setText("¡Botón presionado!");
+    private void guardarUsuario() {
+        // aquí llamas a tu GestorUsuario para registrar
     }
 
+    @FXML
+    private void verificarUsuario() {
+        // aquí llamas a tu GestorUsuario para verificar
+    }
 }
--- Prueba2/src/main/java/Main/MainFX.java
+++ Prueba2/src/main/java/Main/MainFX.java
@@ -1,24 +1,26 @@
 package Main;
-    import javafx.application.Application;
-    import javafx.fxml.FXMLLoader;
-    import javafx.scene.Scene;
-    import javafx.stage.Stage;
 
-    public class MainFX extends Application {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-        @Override
-        public void start(Stage stage) throws Exception {
-            FXMLLoader fxmlLoader = new FXMLLoader(
-                    MainFX.class.getResource("hello-view.fxml")
-            );
-            Scene scene = new Scene(fxmlLoader.load(), 640, 480);
-            stage.setTitle("Mi App JavaFX");
-            stage.setScene(scene);
-            stage.show();
-        }
+public class MainFX extends Application {
 
-        public static void main(String[] args) {
-            launch();
-        }
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(
+                MainFX.class.getResource("/FormularioUsario.fxml")
+        );
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 1100, 600);
+        stage.setTitle("Mi App JavaFX");
+        stage.setScene(scene);
+        stage.show();
     }
 
+    public static void main(String[] args) {
+        launch();
+    }
+}
